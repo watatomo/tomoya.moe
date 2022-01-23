590d795c41ec9cd8e09c970eede447a581531383
Midori Yang <midori@joinmonument.com>
2022-01-22 23:42:26 -0500

    fix toolbar for arbitrary nesting

--- components/tl/Toolbar/index.jsx
+++ components/tl/Toolbar/index.jsx
@@ -8,46 +8,68 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { TextSize } from "../../svgicon";
 import config from "../../../mako.config";
 
+function derivePreviousNextURL({ slug, tl, newSegment }) {
+    const slugSegments = slug.split("/");
+    slugSegments[slugSegments.length - 1] = newSegment;
+    return `/${tl}/${slugSegments.join("/")}`;
+}
+
+function deriveIndexURL({ slug, tl }) {
+    const slugSegments = slug.split("/");
+    slugSegments.pop();
+    return `/${tl}/${slugSegments.join("/")}`;
+}
+
 function Toolbar({ post }) {
     const tl = config.translationsPath || "tl";
+    const isStandaloneChapter = !post.previous && !post.next;
+
     return (
         <div className="toolbar__wrapper">
             <div className="toolbar">
-                {post.previous ? (
-                    <div className="toolbar__section">
-                        <a href={`/${tl}/${post.story}/${post.previous}`}>
+                <div className="toolbar__section">
+                    {post.previous ? (
+                        <a
+                            href={derivePreviousNextURL({
+                                slug: post.slug,
+                                tl,
+                                newSegment: post.previous
+                            })}
+                        >
                             <FontAwesomeIcon icon={faChevronLeft} />
                         </a>
-                    </div>
-                ) : (
-                    <div className="toolbar__section" />
-                )}
+                    ) : null}
+                </div>
 
                 <div className="toolbar__section">
                     <FontAwesomeIcon icon={faFillDrip} />
                 </div>
 
-                {post.story ? (
+                {!isStandaloneChapter && (
                     <div className="toolbar__section">
-                        <a href={`/${tl}/${post.story}`}>
+                        <a href={deriveIndexURL({ slug: post.slug, tl })}>
                             <FontAwesomeIcon icon={faStar} />
                         </a>
                     </div>
-                ) : null}
+                )}
 
                 <div className="toolbar__section">
                     <TextSize />
                 </div>
 
-                {post.next ? (
-                    <div className="toolbar__section">
-                        <a href={`/${tl}/${post.story}/${post.next}`}>
+                <div className="toolbar__section">
+                    {post.next ? (
+                        <a
+                            href={derivePreviousNextURL({
+                                slug: post.slug,
+                                tl,
+                                newSegment: post.next
+                            })}
+                        >
                             <FontAwesomeIcon icon={faChevronRight} />
                         </a>
-                    </div>
-                ) : (
-                    <div className="toolbar__section" />
-                )}
+                    ) : null}
+                </div>
             </div>
         </div>
     );
